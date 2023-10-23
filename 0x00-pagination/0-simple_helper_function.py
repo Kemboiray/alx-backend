cb9b0849df82bd7f7b8aefb4d8e9468ddf7e59e7
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-23 23:34:50 +0300

    Task 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This module defines the function `index_range"""
+"""This module defines the function `index_range`"""
 from typing import Tuple
 
 
@@ -15,3 +15,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 if __name__ == "__main__":
     print("(1, 7) =>", index_range(1, 7))
     print("(3, 15) =>", index_range(3, 15))
+    print("(1, 1) =>", index_range(1, 1))
