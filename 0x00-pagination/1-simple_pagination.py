be48b3a099411e44ef2c52bfde4b987bde0b0b5f
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-10-24 01:08:44 +0300

    Refactor

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,6 +38,6 @@ class Server:
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
-        if start >= len(self.dataset()) or end > len(self.dataset()):
-            return []
+        # if start >= len(self.dataset()) or end > len(self.dataset()):
+        #     return []
         return self.dataset()[start:end]
